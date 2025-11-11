6230fa41edf8149d6610549d4c5c6de7dfee1f11
Rian Moraes <fortpcnite21@gmail.com>
2025-11-11 13:25:00 -0300

    fix: import

--- Models/Model.js
+++ Models/Model.js
@@ -1,6 +1,6 @@
 import Mission from './missionModel.js';
 import User from './userModel.js';
-import UserModel from './userSchema.js';
+import UserModel from './userModel.js';
 
 export default {
   UserModel,
